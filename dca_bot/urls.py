05f5ceea73b075b0e25aa1c8336514c11397a11b
Kyle Salling <ksalling@gmail.com>
2026-02-19 16:54:00 -0800

    root domain fix

--- dca_bot/urls.py
+++ dca_bot/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('dashboard'), name='home'),
+    path('', RedirectView.as_view(url='/core/dashboard/', permanent=False), name='home'),
     path('core/', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls')), # Login/Logout
 ]
