dbd4967f53912ba9a013edea55f289023f231136
Kyle Salling <ksalling@gmail.com>
2026-02-19 17:54:52 -0800

    added email capability with password reset and user registration

---
+++ core/backends.py
@@ -0,0 +1,21 @@
+from django.core.mail.backends.smtp import EmailBackend
+from .models import AppSettings
+
+class DatabaseEmailBackend(EmailBackend):
+    """
+    Custom EmailBackend that reads configuration from the AppSettings model
+    instead of django.conf.settings.
+    """
+    def __init__(self, fail_silently=False, **kwargs):
+        settings = AppSettings.load()
+        
+        kwargs['host'] = settings.smtp_host
+        kwargs['port'] = settings.smtp_port
+        kwargs['username'] = settings.smtp_user
+        kwargs['password'] = settings.smtp_password
+        kwargs['use_tls'] = settings.use_tls
+        # use_ssl is usually mutually exclusive with use_tls for standard django backend init, 
+        # but EmailBackend accepts both. We'll default to False for SSL if TLS is True.
+        kwargs['use_ssl'] = False 
+        
+        super().__init__(fail_silently=fail_silently, **kwargs)
