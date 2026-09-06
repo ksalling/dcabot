a444c441d7da39e12b8db0b420ee8fe206824f6e
ksalling <ksalling@gmail.com>
2026-09-06 09:25:53 -0700

    fix: enable SQLite WAL mode and busy timeout for concurrent background tasks

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+from django.db.backends.signals import connection_created
+
+
+def enable_sqlite_wal(sender, connection, **kwargs):
+    if connection.vendor == 'sqlite':
+        with connection.cursor() as cursor:
+            cursor.execute('PRAGMA journal_mode=WAL;')
+            cursor.execute('PRAGMA busy_timeout=30000;')
+            cursor.execute('PRAGMA synchronous=NORMAL;')
 
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    def ready(self):
+        connection_created.connect(enable_sqlite_wal)
